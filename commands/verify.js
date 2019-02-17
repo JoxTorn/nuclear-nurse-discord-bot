7ca1e1426968ab48cea75a1b5288bcf560c969b2
JoxTorn <joxtorn@gmail.com>
2019-02-17 20:17:49 +0100

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -33,48 +33,39 @@ exports.run = (client, message, args) => {
                             //Check is bot have permission to change nickname
                             if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
                             //Set neickname
-                            membersToVerify.setNickname(name).then(
-                                () => {
+                            membersToVerify.setNickname(name);
 
-                                    //Find verified role and give it to member
-                                    var roleVerified = message.guild.roles.find(role => role.name === client.config.verified_role);
-                                    membersToVerify.addRole(roleVerified).then( () => {
+                            //Find verified role and give it to member
+                            var roleVerified = message.guild.roles.find(role => role.name === client.config.verified_role);
+                            membersToVerify.addRole(roleVerified);
 
-                                        //Check is role exists for user faction
-                                        var roleFaction = message.guild.roles.find(role => role.name === factionName);
-                                        if(roleFaction){
-                                            membersToVerify.addRole(roleFaction);
-                                        }
-                                        else{
-                                            // Create a new role with data
-                                            message.guild.createRole({
-                                                name: factionName
-                                            })
-                                                .then(newRole => {
-                                                    membersToVerify.addRole(newRole);
-                                                })
-                                                .catch(console.error)
-                                        }
+                            //Check is role exists for user faction
+                            var roleFaction = message.guild.roles.find(role => role.name === factionName);
+                            if(roleFaction){
+                                membersToVerify.addRole(roleFaction);
+                            }
+                            else{
+                                // Create a new role with data
+                                message.guild.createRole({
+                                    name: factionName
+                                })
+                                    .then(newRole => {
+                                        membersToVerify.addRole(newRole);
+                                    })
+                                    .catch(console.error)
+                            }
 
-                                        //check is member belong to predefined factons and give roles set in config
-                                        if(client.config.factions[factionID]){
-                                            for(roleToAssign of client.config.factions[factionID]){
-                                                var role = message.guild.roles.find(role => role.name === roleToAssign);
-                                                if(role){
-                                                    membersToVerify.addRole(role);
-                                                }
-                                            }
-                                        }
-                                        //Send welcome message to verified member
-                                        message.channel.send(`Welcome ${membersToVerify}`).catch(console.error);
-
-                                    });
-
-                                    
+                            //check is member belong to predefined factons and give roles set in config
+                            if(client.config.factions[factionID]){
+                                for(roleToAssign of client.config.factions[factionID]){
+                                    var role = message.guild.roles.find(role => role.name === roleToAssign);
+                                    if(role){
+                                        membersToVerify.addRole(role);
+                                    }
                                 }
-                            );
-
-                            
+                            }
+                            //Send welcome message to verified member
+                            message.channel.send(`Welcome ${membersToVerify}`).catch(console.error);
                         } catch (error) {
                             console.log(error);
                             callback(false, "Cant verify user. Error while setting user nickname/roles");
