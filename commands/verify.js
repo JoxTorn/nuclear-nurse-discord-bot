37a84268165bf55222aaf2722e617aaf9a8c7a31
JoxTorn <joxtorn@gmail.com>
2019-02-20 20:55:58 +0100

    Update verify.js

--- commands/verify.js
+++ commands/verify.js
@@ -11,74 +11,76 @@ exports.run = (client, message, args) => {
         membersToVerify = message.guild.members.get(message.mentions.users.first().id);
     }
 
-    //Remove all from member for verification
-    membersToVerify.removeRoles(membersToVerify.roles).then(
-        
-        //calling verification procedure
-        verification.verify(client, membersToVerify.id, function(tornID){
+    if(membersToVerify){
+        //Remove all from member for verification
+        membersToVerify.removeRoles(membersToVerify.roles).then(
+            
+            //calling verification procedure
+            verification.verify(client, membersToVerify.id, function(tornID){
 
-            //checking did i get response with tornID of discord members
-            if(isNaN(tornID)){
-                message.author.send(tornID).catch(console.error);
+                //checking did i get response with tornID of discord members
+                if(isNaN(tornID)){
+                    message.author.send(tornID).catch(console.error);
 
-                if(tornID.indexOf("This Discord account isn't verified by Torn") !== -1){
-                    membersToVerify.send(`Your account couldn't be verified because your Discord account isn't validated by Torn. Please validate your account following these steps:\n1 - Click this link to validate your account in Torn: https://www.torn.com/discord\n2 - Verify your Discord account in our server by running again the command !verify in the channel #door\n`).catch(console.error);
-                    message.reply('I have sent a DM with instructions to validate your account').catch(console.error);
+                    if(tornID.indexOf("This Discord account isn't verified by Torn") !== -1){
+                        membersToVerify.send(`Your account couldn't be verified because your Discord account isn't validated by Torn. Please validate your account following these steps:\n1 - Click this link to validate your account in Torn: https://www.torn.com/discord\n2 - Verify your Discord account in our server by running again the command !verify in the channel #door\n`).catch(console.error);
+                        message.reply('I have sent a DM with instructions to validate your account').catch(console.error);
+                    }
                 }
-            }
-            else{
-                //If i got torn id, continuing with all member data check buy calling user data procedure
-                userData.getUserName(client, tornID, function(verified, name, factionID, factionName){
-                    if(verified){
-                        try {
-                            //Check is bot have permission to change nickname
-                            if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-                            //Set neickname
-                            membersToVerify.setNickname(name);
+                else{
+                    //If i got torn id, continuing with all member data check buy calling user data procedure
+                    userData.getUserName(client, tornID, function(verified, name, factionID, factionName){
+                        if(verified){
+                            try {
+                                //Check is bot have permission to change nickname
+                                if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+                                //Set neickname
+                                membersToVerify.setNickname(name);
 
-                            //Find verified role and give it to member
-                            var roleVerified = message.guild.roles.find(role => role.name === client.config.verified_role);
-                            membersToVerify.addRole(roleVerified);
+                                //Find verified role and give it to member
+                                var roleVerified = message.guild.roles.find(role => role.name === client.config.verified_role);
+                                membersToVerify.addRole(roleVerified);
 
-                            //Check is role exists for user faction
-                            var roleFaction = message.guild.roles.find(role => role.name === factionName);
-                            if(roleFaction){
-                                membersToVerify.addRole(roleFaction);
-                            }
-                            else{
-                                // Create a new role with data
-                                message.guild.createRole({
-                                    name: factionName
-                                })
-                                    .then(newRole => {
-                                        membersToVerify.addRole(newRole);
+                                //Check is role exists for user faction
+                                var roleFaction = message.guild.roles.find(role => role.name === factionName);
+                                if(roleFaction){
+                                    membersToVerify.addRole(roleFaction);
+                                }
+                                else{
+                                    // Create a new role with data
+                                    message.guild.createRole({
+                                        name: factionName
                                     })
-                                    .catch(console.error)
-                            }
+                                        .then(newRole => {
+                                            membersToVerify.addRole(newRole);
+                                        })
+                                        .catch(console.error)
+                                }
 
-                            //check is member belong to predefined factons and give roles set in config
-                            if(client.config.factions[factionID]){
-                                for(roleToAssign of client.config.factions[factionID]){
-                                    var role = message.guild.roles.find(role => role.name === roleToAssign);
-                                    if(role){
-                                        membersToVerify.addRole(role);
+                                //check is member belong to predefined factons and give roles set in config
+                                if(client.config.factions[factionID]){
+                                    for(roleToAssign of client.config.factions[factionID]){
+                                        var role = message.guild.roles.find(role => role.name === roleToAssign);
+                                        if(role){
+                                            membersToVerify.addRole(role);
+                                        }
                                     }
                                 }
+                                //Send welcome message to verified member
+                                message.channel.send(`Welcome ${membersToVerify}`).catch(console.error);
+                            } catch (error) {
+                                console.log(error);
+                                callback(false, "Cant verify user. Error while setting user nickname/roles");
                             }
-                            //Send welcome message to verified member
-                            message.channel.send(`Welcome ${membersToVerify}`).catch(console.error);
-                        } catch (error) {
-                            console.log(error);
-                            callback(false, "Cant verify user. Error while setting user nickname/roles");
                         }
-                    }
-                    else{
-                        message.reply(`${name}`).catch(console.error);
-                    }
-                })
-            }
-            
-        })
-    ).catch(console.error); //Removes all roles
+                        else{
+                            message.reply(`${name}`).catch(console.error);
+                        }
+                    })
+                }
+                
+            })
+        ).catch(console.error); //Removes all roles
+    }
 
 }
