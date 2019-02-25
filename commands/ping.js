81ceccdedf8f2c3a99ca092e12fd90f89169b498
JoxTorn <joxtorn@gmail.com>
2019-02-25 21:14:59 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
     //Ping back
-    //message.channel.send("pong").catch(console.error);
+    message.channel.send("pong").catch(console.error);
     
     // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
     // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    //const m = await message.channel.send("Ping?");
+    //m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
