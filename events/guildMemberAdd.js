93bca9f3964d761b1966e2daab7844f5b694d5dc
JoxTorn <joxtorn@gmail.com>
2019-02-27 18:45:13 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,7 @@
 module.exports = (client, member) => {
    //Send welcome message to defaul channel (default channel is defined in config)
-   member.guild.channels.find(channel => channel.name == client.config.welcome_channel).send(`Welcome <@${member.id}>, please type ${client.config.prefix}verify for verification`); 
+   setTimeout(function() {
+      member.guild.channels.find(channel => channel.name == client.config.welcome_channel).send(`Welcome <@${member.id}>, please type ${client.config.prefix}verify for verification`); 
+   }, 2000);
+   
 }
