91f8aa1695a471a64c5bc1af2aef0fa12a7caafc
JoxTorn <joxtorn@gmail.com>
2019-03-12 23:41:30 +0100

    reordered code for adding roles and changinig nick

--- commands/verify.js
+++ commands/verify.js
@@ -48,6 +48,78 @@ exports.run = (client, message, args) => {
                         userData.getUserName(client, tornID, function(verified, name, factionID, factionName){
                             if(verified){
                                 try {
+
+                                    //NEW CODE ---------------------------------------------------------
+
+                                    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
+                                    //Set neickname
+                                    membersToVerify.setNickname(name).then(m => {
+                                        ///Roles to add
+                                        var roles = [];
+
+                                        //Find verified role and give it to member
+                                        roles.push(message.guild.roles.find(role => role.name === client.config.verified_role));
+
+                                        //check is member belong to predefined factons and give roles set in config
+                                        if(client.config.factions[factionID]){
+                                            for(roleToAssign of client.config.factions[factionID]){
+                                                var role = message.guild.roles.find(role => role.name === roleToAssign);
+                                                if(role){
+                                                    roles.push(role);
+                                                }
+                                            }
+                                        }
+
+                                        //Check is role exists for user faction
+                                        var roleFaction = message.guild.roles.find(role => role.name === factionName);
+                                        if(roleFaction){
+                                            roles.push(roleFaction);
+                                            m.addRoles(roles).then(m => {
+                                                message.channel.send(`Welcome ${m}`).catch(console.error);
+                                            }).catch(error => {
+                                                if(error.code == 50013){
+                                                    message.channel.send(`I don't have permission to modify roles for ${membersToVerify}`).catch(console.error);
+                                                }
+                                                else{
+                                                    console.log(error);
+                                                }
+                                            });
+                                        }
+                                        else{
+                                            // Create a new role with data
+                                            message.guild.createRole({
+                                                name: factionName
+                                            })
+                                                .then(newRole => {
+                                                    roles.push(newRole);
+                                                    m.addRoles(roles).then(m => {
+                                                        message.channel.send(`Welcome ${m}`).catch(console.error);
+                                                    }).catch(error => {
+                                                        if(error.code == 50013){
+                                                            message.channel.send(`I don't have permission to modify roles for ${membersToVerify}`).catch(console.error);
+                                                        }
+                                                        else{
+                                                            console.log(error);
+                                                        }
+                                                    });
+                                                })
+                                                .catch(console.error)
+                                        }
+                                    }).catch(error => {
+                                        if(error.code == 50013){
+                                            message.channel.send(`I don't have permission to modify nickname for ${membersToVerify}`).catch(console.error);
+                                        }
+                                        else{
+                                            console.log(error);
+                                        }
+                                    });
+
+                                    
+
+                                    //OLD CODE ---------------------------------------------------------
+
+                                    /*
+
                                     //Check is bot have permission to change nickname
                                     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
                                     //Set neickname
@@ -84,6 +156,8 @@ exports.run = (client, message, args) => {
                                     }
                                     //Send welcome message to verified member
                                     message.channel.send(`Welcome ${membersToVerify}`).catch(console.error);
+
+                                    */
                                 } catch (error) {
                                     console.log(error);
                                     callback(false, "Cant verify user. Error while setting user nickname/roles");
