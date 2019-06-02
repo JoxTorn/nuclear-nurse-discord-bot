89fedd3f5c5181dfac7db5b55087e1fb84d22af1
JoxTorn <joxtorn@gmail.com>
2019-06-02 23:49:10 +0200

    added command for staock info

---
+++ commands/stockinfo.js
@@ -0,0 +1,26 @@
+const https = require('https');
+
+exports.run = (client, message, args) => {
+      
+    var shares = "";
+
+    for(arg of args){
+        shares += (shares.length > 0 ? "," : "") + arg;
+    }
+
+    var url = `https://www.nukefamily.org/hq/old/StockInfo.php?stocks=${shares}`;
+
+    https.get(url, function(res){
+        var body = '';
+    
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+    
+        res.on('end', function(){
+    
+        });
+    }).on('error', function(error){
+        console.log(error);
+    });
+}
