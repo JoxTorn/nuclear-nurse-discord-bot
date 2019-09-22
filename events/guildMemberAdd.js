ec0f95de7ef76dd5fe8b6b766efb0b7afb40f812
JoxTorn <joxtorn@gmail.com>
2019-09-22 12:11:18 +0200

    added delay on welcome message

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = (client, member) => {
    //Send welcome message to defaul channel (default channel is defined in config)
    setTimeout(function() {
       member.guild.channels.find(channel => channel.name == client.config.welcome_channel).send(`Welcome <@${member.id}>, please type ${client.config.prefix}verify for verification`); 
-   }, 2000);
+   }, 5000);
    
 }
