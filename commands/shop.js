83069caa3c376e8a5a8a5f13670d35af3fdf79c4
Fodilheiro <47642587+Fodilheiro@users.noreply.github.com>
2020-04-10 10:01:39 +0100

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -29,7 +29,7 @@ exports.run = (client, message, args) => {
         let msgEmbed = {
             color: 0x00ff55,
             title: 'Shop',
-            description: `Welcome to **Nuke Rads Shop** where you can exchange your Rads with items`,
+            description: `Welcome to **Nuke Rads Shop** where you can exchange your Rads with items, to buy an item, use the command: **!Buy itemsID**`,
             fields: [],
             timestamp: new Date()
         };
