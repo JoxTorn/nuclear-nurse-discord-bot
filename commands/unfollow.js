89287247a17b4707c5d155934c655e76a3b3ec82
JoxTorn <joxtorn@gmail.com>
2020-04-18 19:09:13 +0200

    follow and unfollow

---
+++ commands/unfollow.js
@@ -0,0 +1,23 @@
+exports.run = (client, message, args) => {
+ 
+    let roles = [];
+    let roleNames = '';
+
+    for(let i = 0; i < args.length; i++){
+        if(client.config.follow_roles.includes(args[i])){
+            roles.push(message.guild.roles.find(role => role.name === args[i]));
+            roleNames += (roleNames == '' ? '' : ', ') + args[i];
+        }
+    }
+
+    message.member.removeRoles(roles).then(m => {
+        message.channel.send(`${m}, you have removed ${roleNames} role${roles.length > 1 ? 's' : ''}.`).catch(console.error);
+    }).catch(error => {
+        if(error.code == 50013){
+            message.channel.send(`I don't have permission to modify roles for ${message.member}`).catch(console.error);
+        }
+        else{
+            console.log(error);
+        }
+    });
+}
