c4f4e732a490543a54078a45da42ead5304a64de
JoxTorn <43649390+JoxTorn@users.noreply.github.com>
2020-04-19 23:11:33 +0200

    Changed timings

--- jobs/loot.js
+++ jobs/loot.js
@@ -4,8 +4,8 @@ exports.run = (client) => {
 
     //Template for jobs, use this code as starting point for new job
     
-    var interval = 1000 /*miliseconds*/ * 60 /*seconds*/ * 0.2 /*minutes*/;
-    var ping_uder = 60 /*seconds*/ * 15 /*minutes*/;
+    var interval = 1000 /*miliseconds*/ * 60 /*seconds*/ * 2 /*minutes*/;
+    var ping_uder = 60 /*seconds*/ * 5 /*minutes*/;
  
     var job = setInterval(checkLoot, interval);
 
