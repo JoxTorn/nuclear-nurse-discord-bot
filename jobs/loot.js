cf9350b12e7c98c1294c0f899fec66f918a2fe73
JoxTorn <43649390+JoxTorn@users.noreply.github.com>
2020-04-20 11:44:29 +0200

    changed check tome to 3min

--- jobs/loot.js
+++ jobs/loot.js
@@ -4,7 +4,7 @@ exports.run = (client) => {
 
     //Template for jobs, use this code as starting point for new job
     
-    var interval = 1000 /*miliseconds*/ * 60 /*seconds*/ * 2 /*minutes*/;
+    var interval = 1000 /*miliseconds*/ * 60 /*seconds*/ * 3 /*minutes*/;
     var ping_uder = 60 /*seconds*/ * 5 /*minutes*/;
  
     var job = setInterval(checkLoot, interval);
