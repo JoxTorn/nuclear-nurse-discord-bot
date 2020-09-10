d7e7ec15319dc49e9c8e72e537222775f97e1c0f
JoxTorn <joxtorn@gmail.com>
2020-09-10 19:29:17 +0200

    style changes

--- commands/elim.js
+++ commands/elim.js
@@ -1,8 +1,9 @@
 const https = require('https');
+const { start } = require('repl');
 
 exports.run = (client, message, args) => {
-    
-    var url = `https://www.nukefamily.org/dev/competition.json`;
+
+    var url = `https://www.nukefamily.org/dev/competition.php`;
 
     https.get(url, function(res){
         var body = '';
@@ -25,7 +26,7 @@ exports.run = (client, message, args) => {
         let msgEmbed = {
             color: 0x00ff55,
             title: data.competition.competition,
-            description: ``,
+            //description: ``,
             fields: [],
             timestamp: new Date()
         };
@@ -34,18 +35,18 @@ exports.run = (client, message, args) => {
         let space = ' ';
 
         data.competition.teams.forEach(element => {
-            logText += ` #${(element.position + space.repeat(3)).slice(0,3)} ${(element.name + space.repeat(20)).slice(0,35)} ${(space.repeat(8) + element.lives).slice(-8)} ${(space.repeat(8) + element.participants).slice(-8)} ${(space.repeat(8) + element.score).slice(-8)} ${(space.repeat(8) + element.wins).slice(-8)} ${(space.repeat(6) + element.losses).slice(-8)} \n`;
+            logText += ` #${(element.position + space.repeat(3)).slice(0,3)} ${(element.name + space.repeat(18)).slice(0,18)} ${(space.repeat(5) + element.lives).slice(-5)} ${(space.repeat(5) + element.score).slice(-5)} ${(space.repeat(8) + element.participants).slice(-8)} ${(space.repeat(5) + element.wins).slice(-5)} ${(space.repeat(5) + element.losses).slice(-5)} \n`;
             //console.log(logText);
         });
 
 
        msgEmbed.fields.push({
             name: '\u200b',
-            value: '```css\n' + `[${('Pos' + space.repeat(3)).slice(0,3)}  ${('Team' + space.repeat(20)).slice(0,20)} ${(space.repeat(8) + 'Lives').slice(-8)} ${(space.repeat(8) + 'Players').slice(-8)} ${(space.repeat(8) + 'Score').slice(-8)} ${(space.repeat(8) + 'Wins').slice(-8)} ${(space.repeat(8) + 'Loses').slice(-8)}]\n` + logText + '```',
+            value: '```css\n' + `[${('Pos' + space.repeat(3)).slice(0,3)}  ${('Team' + space.repeat(18)).slice(0,18)} ${(space.repeat(5) + 'Lives').slice(-5)} ${(space.repeat(5) + 'Score').slice(-5)} ${(space.repeat(8) + 'Players').slice(-8)} ${(space.repeat(5) + 'Wins').slice(-5)} ${(space.repeat(5) + 'Loses').slice(-5)}]\n` + logText + '```',
             inline: true
         });
 
-        message.reply({ embed: msgEmbed });
+        message.channel.send({ embed: msgEmbed });
     }
 
 }
