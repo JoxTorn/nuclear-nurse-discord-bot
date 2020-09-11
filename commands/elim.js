b265c2d1b1a45d56c4b0c99f508324f239d59795
JoxTorn <joxtorn@gmail.com>
2020-09-11 17:41:42 +0200

    added few spaces in message

--- commands/elim.js
+++ commands/elim.js
@@ -35,14 +35,14 @@ exports.run = (client, message, args) => {
         let space = ' ';
 
         data.competition.teams.forEach(element => {
-            logText += ` #${(element.position + space.repeat(3)).slice(0,3)} ${(element.name + space.repeat(18)).slice(0,18)} ${(space.repeat(5) + element.lives).slice(-5)} ${(space.repeat(5) + element.score).slice(-5)} ${(space.repeat(8) + element.participants).slice(-8)} ${(space.repeat(5) + element.wins).slice(-5)} ${(space.repeat(5) + element.losses).slice(-5)} \n`;
+            logText += ` #${(element.position + space.repeat(3)).slice(0,3)} ${(element.name + space.repeat(17)).slice(0,17)} ${(space.repeat(5) + element.lives).slice(-5)} ${(space.repeat(5) + element.score).slice(-5)} ${(space.repeat(8) + element.participants).slice(-8)} ${(space.repeat(6) + element.wins).slice(-6)} ${(space.repeat(6) + element.losses).slice(-6)} \n`;
             //console.log(logText);
         });
 
 
        msgEmbed.fields.push({
             name: '\u200b',
-            value: '```css\n' + `[${('Pos' + space.repeat(3)).slice(0,3)}  ${('Team' + space.repeat(18)).slice(0,18)} ${(space.repeat(5) + 'Lives').slice(-5)} ${(space.repeat(5) + 'Score').slice(-5)} ${(space.repeat(8) + 'Players').slice(-8)} ${(space.repeat(5) + 'Wins').slice(-5)} ${(space.repeat(5) + 'Loses').slice(-5)}]\n` + logText + '```',
+            value: '```css\n' + `[${('Pos' + space.repeat(3)).slice(0,3)}  ${('Team' + space.repeat(17)).slice(0,17)} ${(space.repeat(5) + 'Lives').slice(-5)} ${(space.repeat(5) + 'Score').slice(-5)} ${(space.repeat(8) + 'Players').slice(-8)} ${(space.repeat(6) + 'Wins').slice(-6)} ${(space.repeat(6) + 'Loses').slice(-6)}]\n` + logText + '```',
             inline: true
         });
 
