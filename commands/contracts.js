f14d31e981aabef357647c8b022cd3105bffae7e
Jox <joxtorn@gmail.com>
2022-02-26 13:58:05 +0100

    bugfix

--- commands/contracts.js
+++ commands/contracts.js
@@ -2,6 +2,8 @@ const https = require('https');
 
 exports.run = (client, message, args) => {
 
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" 
+    
     if(message.channel.name !== 'contracts'){
         return message.reply(`Can't execute this command on this channel`);
     }
